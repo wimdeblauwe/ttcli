2cab6dc248efe71afbf8363cbc0717f7e703e468
Wim Deblauwe <wim.deblauwe@gmail.com>
2022-10-23 18:15:45 +0200

    Initial commit

---
+++ src/main/java/io/github/wimdeblauwe/ttcli/TamingThymeleafCliApplication.java
@@ -0,0 +1,13 @@
+package io.github.wimdeblauwe.ttcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamingThymeleafCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamingThymeleafCliApplication.class, args);
+	}
+
+}
