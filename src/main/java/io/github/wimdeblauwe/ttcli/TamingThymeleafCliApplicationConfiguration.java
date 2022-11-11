90cf56b8b7325b2977c2c8c07fd98133d3e8432c
Wim Deblauwe <wim.deblauwe@gmail.com>
2022-11-11 21:01:39 +0100

    feat: Apply custom styling for default values

--- src/main/java/io/github/wimdeblauwe/ttcli/TamingThymeleafCliApplicationConfiguration.java
+++ src/main/java/io/github/wimdeblauwe/ttcli/TamingThymeleafCliApplicationConfiguration.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.shell.jline.PromptProvider;
+import org.springframework.shell.style.FigureSettings;
+import org.springframework.shell.style.StyleSettings;
+import org.springframework.shell.style.Theme;
+import org.springframework.shell.style.ThemeSettings;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -14,6 +18,32 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 @ImportRuntimeHints(TamingThymeleafCliApplicationRuntimeHints.class)
 public class TamingThymeleafCliApplicationConfiguration {
+
+    @Bean
+    Theme myTheme() {
+        return new Theme() {
+            @Override
+            public String getName() {
+                return "taming-thymeleaf-theme";
+            }
+
+            @Override
+            public ThemeSettings getSettings() {
+                return new ThemeSettings() {
+                    @Override
+                    public StyleSettings styles() {
+                        return new TamingThymeleafStyleSettings();
+                    }
+
+                    @Override
+                    public FigureSettings figures() {
+                        return FigureSettings.defaults();
+                    }
+                };
+            }
+        };
+    }
+
     @Bean
     public PromptProvider myPromptProvider() {
         return () -> new AttributedString("tt> ", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
---
+++ src/main/java/io/github/wimdeblauwe/ttcli/TamingThymeleafStyleSettings.java
@@ -0,0 +1,10 @@
+package io.github.wimdeblauwe.ttcli;
+
+import org.springframework.shell.style.StyleSettings;
+
+public class TamingThymeleafStyleSettings extends StyleSettings {
+    @Override
+    public String value() {
+        return "fg-rgb:#6b6b6b";
+    }
+}
