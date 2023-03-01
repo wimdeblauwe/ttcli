1a410ce0d7609c2ffa2b2f296d19dae95c7c64af
Wim Deblauwe <wim.deblauwe@gmail.com>
2023-03-01 21:23:27 +0100

    Feat: Support Shoelace as a webjar
    Fixes #14

---
+++ src/main/java/io/github/wimdeblauwe/ttcli/deps/ShoelaceWebDependency.java
@@ -0,0 +1,40 @@
+package io.github.wimdeblauwe.ttcli.deps;
+
+import io.github.wimdeblauwe.ttcli.maven.MavenDependency;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * See <a href="https://shoelace.style/">https://shoelace.style/</a>
+ */
+@Component
+public class ShoelaceWebDependency implements WebDependency {
+    @Override
+    public String id() {
+        return "shoelace";
+    }
+
+    @Override
+    public String displayName() {
+        return "Shoelace";
+    }
+
+    @Override
+    public List<MavenDependency> getMavenDependencies(String springBootVersion) {
+        return Collections.singletonList(new MavenDependency("org.webjars.npm", "shoelace-style__shoelace", "2.2.0"));
+    }
+
+    @Override
+    public String getCssLinksForLayoutTemplate() {
+        return """
+                <link rel="stylesheet" th:href="@{/webjars/shoelace-style__shoelace/dist/themes/light.css}">""";
+    }
+
+    @Override
+    public String getJsLinksForLayoutTemplate() {
+        return """
+                <script type="module" th:src="@{/webjars/shoelace-style__shoelace/dist/shoelace.js}"></script>""";
+    }
+}
