2fbf7840481f9c3cd01a32953b97db20af9953ed
Wim Deblauwe <wim.deblauwe@gmail.com>
2023-04-22 13:27:22 +0200

    fix: Show error message at the end if there was a problem during project generation
    Fixes #26
    
    (cherry picked from commit 409b1eb68679a5c192c912481186c6e5dbfa31fc)

---
+++ src/main/java/io/github/wimdeblauwe/ttcli/ProjectInitializationServiceException.java
@@ -0,0 +1,7 @@
+package io.github.wimdeblauwe.ttcli;
+
+public class ProjectInitializationServiceException extends RuntimeException {
+    public ProjectInitializationServiceException(String message) {
+        super(message);
+    }
+}
