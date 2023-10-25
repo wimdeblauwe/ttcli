6c2868a18f52e2eee383f82f58c22d710620bdb7
Wim Deblauwe <wim.deblauwe@gmail.com>
2023-10-25 17:08:09 +0200

    build: Update to AlpineJS 3.13.2
    Fixes #78

--- src/main/java/io/github/wimdeblauwe/ttcli/deps/AlpineWebDependency.java
+++ src/main/java/io/github/wimdeblauwe/ttcli/deps/AlpineWebDependency.java
@@ -20,7 +20,7 @@ public class AlpineWebDependency implements WebDependency {
 
     @Override
     public List<MavenDependency> getMavenDependencies(String springBootVersion) {
-        return Collections.singletonList(new MavenDependency("org.webjars.npm", "alpinejs", "3.13.0"));
+        return Collections.singletonList(new MavenDependency("org.webjars.npm", "alpinejs", "3.13.2"));
     }
 
     @Override
