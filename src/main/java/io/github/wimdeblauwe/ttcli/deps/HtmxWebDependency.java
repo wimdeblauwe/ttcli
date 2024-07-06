1ada53346c5969688c3d0182ea25ffc84b51a80b
Wim Deblauwe <wim.deblauwe@gmail.com>
2024-07-06 10:56:30 +0200

    build: Update to htmx-spring-boot 3.4.1
    Fixes #136

--- src/main/java/io/github/wimdeblauwe/ttcli/deps/HtmxWebDependency.java
+++ src/main/java/io/github/wimdeblauwe/ttcli/deps/HtmxWebDependency.java
@@ -44,7 +44,7 @@ public class HtmxWebDependency implements WebDependency {
         if (springBootVersion.startsWith("2.")) {
             htmxSpringBootThymeleafVersion = "1.0.0";
         } else if (springBootVersion.startsWith("3.")) {
-            htmxSpringBootThymeleafVersion = "3.2.0";
+            htmxSpringBootThymeleafVersion = "3.4.1";
         } else {
             throw new IllegalArgumentException("Unknown Spring Boot version: " + springBootVersion);
         }
