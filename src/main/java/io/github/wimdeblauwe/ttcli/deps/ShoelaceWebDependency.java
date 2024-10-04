09328181211191a1ef263192ba54fbbe29d4bc3c
Wim Deblauwe <wim.deblauwe@gmail.com>
2024-10-04 11:55:24 +0200

    build: Update to Shoelace 2.17.1
    Fixes #164

--- src/main/java/io/github/wimdeblauwe/ttcli/deps/ShoelaceWebDependency.java
+++ src/main/java/io/github/wimdeblauwe/ttcli/deps/ShoelaceWebDependency.java
@@ -23,7 +23,7 @@ public class ShoelaceWebDependency implements WebDependency {
 
     @Override
     public List<MavenDependency> getMavenDependencies(String springBootVersion) {
-        return Collections.singletonList(new MavenDependency("org.webjars.npm", "shoelace-style__shoelace", "2.15.1"));
+        return Collections.singletonList(new MavenDependency("org.webjars.npm", "shoelace-style__shoelace", "2.17.1"));
     }
 
     @Override
