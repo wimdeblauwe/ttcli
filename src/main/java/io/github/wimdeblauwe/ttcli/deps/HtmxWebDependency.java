d319a19ab2193559ca3e956019b0cc5557acb64f
Wim Deblauwe <wim.deblauwe@gmail.com>
2024-11-19 20:16:04 +0100

    build: Update to htmx 2.0.3
    Fixes #176

--- src/main/java/io/github/wimdeblauwe/ttcli/deps/HtmxWebDependency.java
+++ src/main/java/io/github/wimdeblauwe/ttcli/deps/HtmxWebDependency.java
@@ -23,7 +23,7 @@ public class HtmxWebDependency implements WebjarsBasedWebDependency {
         String htmxSpringBootThymeleafVersion = getHtmxSpringBootThymeleafVersion(springBootVersion);
 
         return List.of(
-                new MavenDependency("org.webjars.npm", "htmx.org", "2.0.2"),
+                new MavenDependency("org.webjars.npm", "htmx.org", "2.0.3"),
                 new MavenDependency("io.github.wimdeblauwe", "htmx-spring-boot-thymeleaf", htmxSpringBootThymeleafVersion)
         );
     }
